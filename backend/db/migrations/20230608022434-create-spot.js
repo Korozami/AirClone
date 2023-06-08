10e73b2276a4866c0668ec6da5eafec930b61697
Kevin Sy <kevsy@ucdavis.edu>
2023-06-07 19:41:35 -0700

    created spot model and migration

---
+++ backend/db/migrations/20230608022434-create-spot.js
@@ -0,0 +1,72 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+ options.schema = process.env.SCHEMA; // define your schema in options object
+}
+options.tableName = "Spots";
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Spots', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      ownerId: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      address: {
+        type: Sequelize.STRING
+      },
+      city: {
+        type: Sequelize.STRING
+      },
+      state: {
+        type: Sequelize.STRING
+      },
+      country: {
+        type: Sequelize.STRING
+      },
+      lat: {
+        type: Sequelize.INTEGER
+      },
+      lng: {
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      price: {
+        type: Sequelize.INTEGER
+      },
+      avgRating: {
+        type: Sequelize.INTEGER
+      },
+      previewImage: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
+      }
+    }, options);
+  },
+  async down(queryInterface, Sequelize) {
+    options.tableName = 'Spots'
+    await queryInterface.dropTable(options);
+  }
+};
